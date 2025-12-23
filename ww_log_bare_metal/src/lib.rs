5179e7ae5355821ab8ed61055cb5a9455f7f8c6a
Roman Isaikin <romix.lab@gmail.com>
2025-12-23 20:13:47 +0300

    feat: prototype Counters trait

--- ww_log_bare_metal/src/lib.rs
+++ ww_log_bare_metal/src/lib.rs
@@ -2,3 +2,14 @@ use wire_weaver::prelude::*;
 
 #[ww_trait]
 pub trait Log {}
+
+#[derive_shrink_wrap]
+#[ww_repr(u4)]
+pub enum Severity {
+    Error,
+    Warn,
+    Info,
+    Debug,
+    Trace,
+    User(Nibble)
+}
